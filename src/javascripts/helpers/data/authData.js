66b042e7c9c4b47ced5aec023d154f609716944a
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-03-23 20:32:03 -0500

    farmers printing

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -2,9 +2,11 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import pasture from '../../components/pasture/pasture';
+import farmhouse from '../../components/farmhouse/farmhouse';
 
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
+const farmhouseDiv = $('#farmhouse');
 const logoutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
@@ -14,12 +16,15 @@ const checkLoginStatus = () => {
       authDiv.addClass('hide');
       pastureDiv.removeClass('hide');
       logoutButton.removeClass('hide');
+      farmhouseDiv.removeClass('hide');
       pasture.buildCows();
+      farmhouse.buildFarmers();
     } else {
       // person is NOT logged in
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
       logoutButton.addClass('hide');
+      farmhouseDiv.addClass('hide');
     }
   });
 };
